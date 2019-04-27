2863123fe439baaf70105f37866a94dd5362d658
jainankit102 <jainankit102@users.noreply.github.com>
2019-04-27 14:23:21 +0530

    Added auto refresh

--- public/js/app.js
+++ public/js/app.js
@@ -10,6 +10,10 @@ const inputEle = document.querySelector('input');
 const errorMessge = document.getElementById('errorMessage');
 const result = document.getElementById('summary');
 const details = document.getElementById('details');
+const autoRefreshCheckbox = document.getElementById('autoRefresh');
+
+let lastAPIHitTime;
+let autoRefreshInterval;
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -17,40 +21,66 @@ weatherForm.addEventListener('submit', (event) => {
     if (!address.trim()) {
         errorMessge.textContent = 'Please provide the locatio'
     } else {
+        lastAPIHitTime = new Date();
         result.textContent = 'Loading...'
-        getData(address, (response) => {
-            if (response.hasOwnProperty('error')) {
-                errorMessge.textContent = response.error;
-                result.textContent = ''
-            } else {
-                // result.textContent = response.location + '\n' + response.tempreture;
-                const text = `<p> ${response.location} </p><p> ${response.tempreture} </p> 
-                <table class="key-value-table" summary="Wrather details.">
-                <caption>Weather details</caption>
-                <tbody>
-                    <tr>
-                        <th class="width-20">Max Temmpreature:</th>
-                        <td>${response.maxTemepratue}</td>
-                    </tr>
-                    <tr>
-                        <th>Min Tempreture</th>
-                        <td>${response.minTemepratue}</td>
-                    </tr>
-                    <tr>
-                        <th>Wind speed</th>
-                        <td>${response.windSpeed}</td>
-                    </tr>
-                    <tr>
-                        <th>Humidity</th>
-                        <td>${response.humidity}</td>
-                    </tr>
-                </tbody>
-            </table>`;
-            details.innerHTML = text;
-            }
-        });
+        updateDetails(address);
     }
 })
+autoRefreshCheckbox.addEventListener('change', (event) => {
+    event.preventDefault();
+    const status = event.target.checked;
+   
+    if (status) {
+        autoRefreshInterval = setInterval(() => {
+            const address = inputEle.value;
+            if (address) {
+                updateDetails(address);
+            }
+        }, 10000)
+    } else {
+        clearInterval(autoRefreshInterval);
+    }
+})
+
+
+function updateDetails(address) {
+    getData(address, (response) => {
+        result.textContent = ''
+        if (response.hasOwnProperty('error')) {
+            errorMessge.textContent = response.error;
+        } else {
+            const dateObj = new Date()
+            const text = `<p> ${response.location} </p><p> ${response.tempreture} </p>
+            <table class="key-value-table" summary="Wrather details.">
+            <caption>Weather details</caption>
+            <tbody>
+                <tr>
+                    <th>Current Time</th>
+                    <td>${dateObj.getHours()}:${dateObj.getMinutes()}:${dateObj.getSeconds()}</td>
+                </tr>
+                <tr>
+                    <th class="width-20">Max Temmpreature</th>
+                    <td>${response.maxTemepratue}</td>
+                </tr>
+                <tr>
+                    <th>Min Tempreture</th>
+                    <td>${response.minTemepratue}</td>
+                </tr>
+                <tr>
+                    <th>Wind speed</th>
+                    <td>${response.windSpeed}</td>
+                </tr>
+                <tr>
+                    <th>Humidity</th>
+                    <td>${response.humidity}</td>
+                </tr>
+            </tbody>
+        </table>`;
+            details.innerHTML = text;
+        }
+    });
+}
+
 
 const getData = (address, callback) => {
 
